e6d3c9e08470567886487f1a7d7231c491f48b16
TatianaLouise <tloropel@gmail.com>
2023-12-16 07:32:43 -0800

    final push

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -67,18 +67,84 @@ def precipitation():
 
     return jsonify(data)
 
+# Return a JSON list of stations from the dataset.
+@app.route("/api/v1.0/stations")
+def stations():
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
     # Return a JSON list of stations from the dataset.
 
-    @app.route("/api/v1.0/stations")
-    def stations():
-    
+    station_activity = session.query(Measurements.station, func.count(Measurements.station)).\
+    group_by(Measurements.station).order_by(func.count(Measurements.station).desc()).all()
 
-    # Query the dates and temperature observations of the most-active station for the previous year of data.
+    session.close()
 
+    # Convert tuples into a list
+
+    station_list = list(np.ravel(station_activity))
+
+    return jsonify(station_list)
+
+# Query the dates and temperature observations of the most-active station for the previous year of data.
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
    
+    active_station_data = session.query(Measurements.date, Measurements.tobs).filter(Measurements.station == 'USC00519281')
+
+    session.close()
+    
+    # Convert the list to Dictionary
+    all_tobs = []
+
+    for date,tobs in active_station_data:
+
+        tobs_dict = {}
+
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        
+        all_tobs.append(tobs_dict)
 
 
-    # 4. Define main behavior
+    return jsonify(all_tobs)
+
+# Return a JSON list of the minimum temperature, 
+# the average temperature, and the maximum temperature for a specified start or start-end range.
+
+@app.route("/api/v1.0/<start>")
+def start():
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    date_statistics = session.query(func.min(Measurements.tobs), func.avg(Measurements.tobs), func.max(Measurements.tobs)).\
+                filter(Measurements.date >= first_date).all()
+    
+    session.close()
+
+    # Convert data to a list of dictionaries and jsonify
+
+    start_date_tobs = []
+
+    for min, avg, max in date_statistics:
+
+        tobs_dict = {}
+
+        start_date_tobs_dict["min_temp"] = min
+        start_date_tobs_dict["avg_temp"] = avg
+        start_date_tobs_dict["max_temp"] = max
+
+        start_date_tobs.append(tobs_dict
+                               ) 
+        
+    return jsonify(start_date_tobs)
+
+# 4. Define main behavior
 if __name__ == "__main__":
     app.run(debug=True)
